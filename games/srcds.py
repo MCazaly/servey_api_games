48678cc161923c3093aead501284ef3a270d5023
Fakas <no_email>
2020-02-09 13:39:57 +0000

    Implement Srcds class

--- games/srcds.py
+++ games/srcds.py
@@ -1,8 +1,26 @@
-from flask_restplus import Resource
+from .game import Game
+from socket import timeout as socket_timeout
+import a2s
 
-class Srcds(Resource):
-    api = None
 
-    def __init__(self, api):
+class Srcds(Game):
+    def __init__(self, address):
         super().__init__()
-        self.api = api
+        try:
+            srcds_info = a2s.info(tuple(address))
+        except socket_timeout:
+            return
+        self.info.online = True
+        self.info.server_name = srcds_info.server_name
+        self.info.game_name = srcds_info.game
+        self.info.map_name = srcds_info.map_name
+        self.info.player_count = srcds_info.player_count
+        self.info.max_player_count = srcds_info.max_players
+        self.info.srcds = {
+            "folder": srcds_info.folder,
+            "app_id": srcds_info.app_id,
+            "bot_count": srcds_info.bot_count,
+            "password_protected": srcds_info.password_protected,
+            "vac_enabled": srcds_info.vac_enabled,
+            "version": srcds_info.version,
+        }
