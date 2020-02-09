d17d3503c04b69d984f491ae5b826aff7142b7e9
Fakas <no_email>
2020-02-09 13:40:27 +0000

    Implement Flask modules

--- __init__.py
+++ __init__.py
@@ -1,9 +1,23 @@
 from flask import Flask
+from flask_restplus import Resource, Api
+import games
 
-app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Internal API!"
+
+class Application(object):
+    name = "ServeyMcServeface API"
+    app = Flask(name)
+    api = Api(app)
+    api.title = name
+    root = "/api/"
+
+    def run(self):
+        return self.app.run()
+
+    def add_namespace(self, namespace):
+        return self.api.add_namespace(namespace)
+
+
 if __name__ == "__main__":
-    app.run()
-
+    flask_app = Application()
+    flask_app.add_namespace(games.api)
+    flask_app.run()
---
+++ games/__init__.py
@@ -0,0 +1,38 @@
+import json
+from flask_restplus import Namespace, Resource, fields
+from .srcds import Srcds
+from .minecraft import Minecraft
+from os import path
+
+
+directory = path.dirname(path.abspath(__file__))
+games_path = path.join(path.split(path.dirname(path.abspath(__file__)))[0], "games.json")
+if not path.isfile(games_path):
+    with open(games_path, "w") as file:
+        file.write("{}")
+with open(games_path, "r") as file:
+    games = json.load(file)
+
+sources = {
+    "srcds": Srcds,
+    "minecraft": Minecraft
+}
+
+api = Namespace("games", "Gameserver-related operations")
+
+
+@api.route("/")
+class GameList(Resource):
+    @staticmethod
+    def get():
+        return list(games.keys())
+
+
+@api.route("/<string:game_id>")
+class GameInfo(Resource):
+    @staticmethod
+    @api.doc("Get game info")
+    def get(game_id):
+        game = games[game_id]
+        source = sources[game["source"]](game["address"])
+        return source.get_dict()
