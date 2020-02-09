abac5f52bea0daf9dfb77b23ec3267b002d5c955
Fakas <no_email>
2020-02-09 14:10:39 +0000

    Refactor main application

--- __init__.py
+++ __init__.py
@@ -1,26 +1,18 @@
 from flask import Flask
-from flask_restplus import Resource, Api
+from flask_restplus import Api
 from . import games
 
+name = "ServeyMcServeface API"
+app = Flask(name)
+api = Api(app)
+api.title = name
+root = "/api/"
 
-class Application(object):
-    name = "ServeyMcServeface API"
-    app = Flask(name)
-    api = Api(app)
-    api.title = name
-    root = "/api/"
-
-    def run(self):
-        return self.app.run()
-
-    def add_namespace(self, namespace):
-        return self.api.add_namespace(namespace)
+api.add_namespace(games.api)
 
 
 def main():
-    flask_app = Application()
-    flask_app.add_namespace(games.api)
-    flask_app.run()
+    app.run()
 
 
 if __name__ == "__main__":
