bfbc60468930295c336508fabd15dc2084c454ea
Fakas <no_email>
2020-03-08 11:02:52 +0000

    Refactor games to root

--- __init__.py
+++ __init__.py
@@ -1,15 +1,61 @@
 from flask import Flask, Blueprint
-from flask_restplus import Api
+from flask_restplus import Api, Resource
 from . import games
+from os import path
+import json
+from .games import Srcds
+from .games import Minecraft
 
 name = "ServeyMcServeface API"
 app = Flask(name)
-app.config["SERVER_NAME"] = "api.serveymcserveface.com:80"
-app.config["APPLICATION_ROOT"] = "/games"
+# TODO app.config["SERVER_NAME"] = "api.serveymcserveface.com:80"
+app.config["TESTING"] = True
+
 api = Api(app)
 api.title = name
 
-api.add_namespace(games.api)
+directory = path.dirname(path.abspath(__file__))
+games_path = path.join(path.dirname(path.abspath(__file__)), "games.json")
+if not path.isfile(games_path):
+    with open(games_path, "w") as file:
+        file.write("{}")
+with open(games_path, "r") as file:
+    games = json.load(file)
+
+sources = {
+    "srcds": Srcds,
+    "minecraft": Minecraft
+}
+
+
+@api.route("/list")
+class GameList(Resource):
+    @staticmethod
+    def get():
+        return list(games.keys())
+
+
+@api.route("/all")
+class GameInfoAll(Resource):
+    @staticmethod
+    @api.doc("Get info for all games")
+    def get():
+        game_info = {}
+        for game_id in games.keys():
+            game = games[game_id]
+            source = sources[game["source"]](game["address"])
+            game_info[game_id] = source.get_dict()
+        return game_info
+
+
+@api.route("/game/<string:game_id>")
+class GameInfo(Resource):
+    @staticmethod
+    @api.doc("Get game info")
+    def get(game_id):
+        game = games[game_id]
+        source = sources[game["source"]](game["address"])
+        return source.get_dict()
 
 
 def main():
