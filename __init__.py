c5fa300488e1cb21e811340144e015a87a8c9259
Fakas <no_email>
2020-03-08 10:41:54 +0000

    Flask is dumb.

--- __init__.py
+++ __init__.py
@@ -2,16 +2,17 @@ from flask import Flask, Blueprint
 from flask_restplus import Api
 from . import games
 
+name = "ServeyMcServeface API"
+app = Flask(name)
+app.config["SERVER_NAME"] = "api.serveymcserveface.com:80"
+app.config["APPLICATION_ROOT"] = "/games"
+api = Api(app)
+api.title = name
+
+api.add_namespace(games.api)
+
 
 def main():
-    name = "ServeyMcServeface API"
-    app = Flask(name)
-    app.config["SERVER_NAME"] = "api.serveymcserveface.com:80"
-    app.config["APPLICATION_ROOT"] = "/games"
-    api = Api(app)
-    api.title = name
-
-    api.add_namespace(games.api)
     app.run()
 
 
