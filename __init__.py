ac116882933ff24ac0b5a30e1d6b2888d0cdec16
Fakas <no_email>
2020-03-08 10:37:16 +0000

    Exploratory work on API refactor

--- __init__.py
+++ __init__.py
@@ -1,17 +1,17 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_restplus import Api
 from . import games
 
-name = "ServeyMcServeface API"
-app = Flask(name)
-api = Api(app)
-api.title = name
-root = "/api/"
-
-api.add_namespace(games.api)
-
 
 def main():
+    name = "ServeyMcServeface API"
+    app = Flask(name)
+    app.config["SERVER_NAME"] = "api.serveymcserveface.com:80"
+    app.config["APPLICATION_ROOT"] = "/games"
+    api = Api(app)
+    api.title = name
+
+    api.add_namespace(games.api)
     app.run()
 
 
